e127bdef84fc728dc1d80337d652fa6a61a1ad06
Elton Rodrigues <el_dantas1999@hotmail.com>
2022-06-05 00:26:02 -0300

    Criação do car.service.ts

---
+++ src/services/car.service.ts
@@ -0,0 +1,17 @@
+import Service, { ServiceError } from '.';
+import CarModel from '../models/CarModel';
+import CarInterface, { Car } from '../interfaces/CarInterface';
+
+export default class CarService extends Service<Car> {
+  constructor(model = new CarModel()) {
+    super(model);
+  }
+
+  async create(obj: Car): Promise<Car | ServiceError | null> {
+    const car = CarInterface.safeParse(obj);
+    if (!car.success) {
+      return { error: car.error };
+    }
+    return this.model.create(obj);
+  }
+}
