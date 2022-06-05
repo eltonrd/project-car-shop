a272901da0daac70a0217558fb9f80aba7083dbe
Elton Rodrigues <el_dantas1999@hotmail.com>
2022-06-05 19:17:44 -0300

    Criação do index.ts do controller

---
+++ src/controllers/index.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import Service from '../services';
+
+export type ResponseError = {
+  error: unknown;
+};
+
+export interface RequestWithBody<T> extends Request {
+  body: T;
+}
+
+enum ControllerErrors {
+  internal = 'Internal Server Error',
+  notFound = 'Object not found',
+  requiredId = 'Id is required',
+  badRequest = 'Bad request',
+}
+
+export default abstract class Controller<T> {
+  abstract route:string;
+
+  public static error = ControllerErrors;
+
+  constructor(protected service: Service<T>) {}
+
+  public abstract create(
+    req: RequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+}
